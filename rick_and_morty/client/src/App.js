415b23098fec5c7f304498294f77312b3fdfd967
Lautaro <laucarrizo332@gmail.com>
2023-07-18 23:21:16 -0300

    Agregando HM ASYNC

--- rick_and_morty/client/src/App.js
+++ rick_and_morty/client/src/App.js
@@ -1,89 +1,108 @@
-import './css/App.css';
-import { useState, useEffect } from 'react';
-import axios from "axios"
-import Nav from './components/componentesHijos/nav';
-import {Routes, Route, useLocation, useNavigate} from "react-router-dom"
-import Details from './components/componentesHijos/detail';
-import Abaout from './components/comoponentesPadres/Abaout';
-import Forms from './components/comoponentesPadres/forms';
-import Home from './components/comoponentesPadres/home';
-import Favorites from './components/comoponentesPadres/favorites';
-import Footer from './components/componentesHijos/footer';
+import "./css/App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Nav from "./components/componentesHijos/nav";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import Details from "./components/componentesHijos/detail";
+import Abaout from "./components/comoponentesPadres/Abaout";
+import Forms from "./components/comoponentesPadres/forms";
+import Home from "./components/comoponentesPadres/home";
+import Favorites from "./components/comoponentesPadres/favorites";
+import Footer from "./components/componentesHijos/footer";
 //import users from '../../server/src/utils/users';
 
 function App() {
-
   const [characters, setCharacters] = useState([]);
- 
-function onSearch(id) {
-   axios(`http://localhost:3001/rickandmorty/character/${id}`).then(({ data }) => {
+
+  async function onSearch(id) {
+    try {
+      const res = await axios(
+        `http://localhost:3001/rickandmorty/character/${id}`
+      );
+      const { data } = res;
       if (data.name) {
-         setCharacters((oldChars) => [...oldChars, data]);
+        setCharacters((oldChars) => [...oldChars, data]);
       } else {
-         window.alert('¡No hay personajes con este ID!');
+        window.alert("¡No hay personajes con este ID!");
       }
-   });
-}
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
 
-function onSearchRandom(randomId) {
-   axios(`https://rickandmortyapi.com/api/character/${randomId}`).then(({ data }) => {
-      if (data.name) {
-         setCharacters((oldChars) => [...oldChars, data]);
-      } else {
-         window.alert('¡No hay personajes con este ID!');
+  function onSearchRandom(randomId) {
+    axios(`https://rickandmortyapi.com/api/character/${randomId}`).then(
+      ({ data }) => {
+        if (data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert("¡No hay personajes con este ID!");
+        }
       }
-   });
-}
+    );
+  }
 
-function handleRandomId(){
-   let calculo = Math.random() * 100
-   let randomId = Math.floor(calculo % 826)
-   onSearchRandom(randomId)
-}
+  function handleRandomId() {
+    let calculo = Math.random() * 100;
+    let randomId = Math.floor(calculo % 826);
+    onSearchRandom(randomId);
+  }
 
-function onClose(id){
-   setCharacters((characters) =>
-    characters.filter((elem) => elem.id !== Number(id))
-  );
-}
-//? FLUJO
-///! FRONT => ACTION PETICION DISPATCH ==> BACK => CONTROLLER DESECTRURAR DATA => DEVUELVE POR MEDIO DE ROUTES A ESA URL, SE RESUELVE LA PROMISE ==> MANDA AL REDUCER ACTUALIZA EL ESTADO =>> ACUTALIZA LAS STORE.
-let ubi = useLocation()
+  function onClose(id) {
+    setCharacters((characters) =>
+      characters.filter((elem) => elem.id !== Number(id))
+    );
+  }
+  //? FLUJO
+  ///! FRONT => ACTION PETICION DISPATCH ==> BACK => CONTROLLER DESECTRURAR DATA => DEVUELVE POR MEDIO DE ROUTES A ESA URL, SE RESUELVE LA PROMISE ==> MANDA AL REDUCER ACTUALIZA EL ESTADO =>> ACUTALIZA LAS STORE.
+  let ubi = useLocation();
 
-const [access, setAccess] = useState(false)
-let navigate = useNavigate()
-function login(userData) {
-   const { email, password } = userData;
-   const URL = 'http://localhost:3001/rickandmorty/login/';
-   axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+  const [access, setAccess] = useState(false);
+  let navigate = useNavigate();
+  function login(userData) {
+    const { email, password } = userData;
+    const URL = "http://localhost:3001/rickandmorty/login/";
+    axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
       const { access } = data;
       setAccess(access);
-      access && navigate('/home');
-   });
-}
+      access && navigate("/home");
+    });
+  }
 
-useEffect(() => {
-   !access && navigate('/');
-}, [access, navigate]);
+  useEffect(() => {
+    !access && navigate("/");
+  }, [access, navigate]);
 
-   return ( 
-      <div className='App'>
-                 {ubi.pathname !== "/" && (
-        <Nav access={access} setAccess={setAccess} showNav={ubi.pathname !== "/"} />
-    )}
-         <Routes>
-            <Route path='/' element={<Forms login={login} />}></Route>
-            <Route path='/home' element= {<Home onClose={onClose} characters={characters} handleRandomId={handleRandomId} onSearch={onSearch}/>} ></Route>
-            <Route path='/Abaout' element= {<Abaout/>}></Route>
-            <Route path='/detail/:id' element= {<Details/>} ></Route>
-            <Route path='/favorites' element= {<Favorites/>}></Route>
-         </Routes>
+  return (
+    <div className="App">
+      {ubi.pathname !== "/" && (
+        <Nav
+          access={access}
+          setAccess={setAccess}
+          showNav={ubi.pathname !== "/"}
+        />
+      )}
+      <Routes>
+        <Route path="/" element={<Forms login={login} />}></Route>
+        <Route
+          path="/home"
+          element={
+            <Home
+              onClose={onClose}
+              characters={characters}
+              handleRandomId={handleRandomId}
+              onSearch={onSearch}
+            />
+          }
+        ></Route>
+        <Route path="/Abaout" element={<Abaout />}></Route>
+        <Route path="/detail/:id" element={<Details />}></Route>
+        <Route path="/favorites" element={<Favorites />}></Route>
+      </Routes>
 
-         {ubi.pathname !== "/" && (
-           <Footer/>
-    )}
-      </div>
-   );
+      {ubi.pathname !== "/" && <Footer />}
+    </div>
+  );
 }
 
 export default App;
--- rick_and_morty/server/src/controllers/getCharByid.js
+++ rick_and_morty/server/src/controllers/getCharByid.js
@@ -1,30 +1,28 @@
 const axios = require("axios");
 const URL = "https://rickandmortyapi.com/api/character/";
 
-function getCharByid (req, res)  {
+async function getCharByid(req, res) {
   const { id } = req.params;
-
-  axios
-    .get(`${URL}/${id}`)
-    .then(({ status, data }) => {
-      if (status === 200 && data && data.id) {
-        const character = {
-          id: data.id,
-          status: data.status,
-          name: data.name,
-          species: data.species,
-          origin: data.origin?.name,
-          image: data.image,
-          gender: data.gender,
-        };
-        return res.json(character);
-      } else {
-        return res.status(404).json({ message: "Not Found" });
-      }
-    })
-    .catch((error) => {
-      return res.status(500).json({ message: error });
-    });
-};
+  try {
+    const response = await axios.get(`${URL}/${id}`);
+    const { status, data } = response;
+    if (status === 200 && data && data.id) {
+      const character = {
+        id: data.id,
+        status: data.status,
+        name: data.name,
+        species: data.species,
+        origin: data.origin?.name,
+        image: data.image,
+        gender: data.gender,
+      };
+      return res.json(character);
+    } else {
+      return res.status(404).json({ message: "Not Found" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
 
 module.exports = getCharByid;
